4315f9e94ef5c6f65ec9b19425354b0bcef6d0c1
METADEV <80870883+metadev999@users.noreply.github.com>
2022-11-02 11:31:16 -0700

    qweqwe

--- nftsklcol.sol
+++ nftsklcol.sol
@@ -2,28 +2,33 @@
 
  
 
+
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
+
 pragma solidity ^0.8.4;
 
-contract Collection is ERC721Enumerable, Ownable {
+contract SkullsCollection is ERC721Enumerable, Ownable {
 
     
     using Strings for uint256;
     string public baseURI;
     string public baseExtension = ".json";
     uint256 public maxSupply = 2000;
+    uint256 public cost = 0.005 ether;
     uint256 public maxMintAmount = 10;
     bool public paused = false;
 
-    constructor() ERC721("BoredSkullsClub", "BSKULLCOL") {}
+    constructor() ERC721("BoredSkullsClub", "BCSCOL") {}
 
 
     function _baseURI() internal view virtual override returns (string memory) {
     return "ipfs://Qmde6GY5f8xUfzimfFPZ7iSgo2yFeJTQPa2ERwXPSBHe6F/";
 
     }
+
+
     
     function mint(address _to, uint256 _mintAmount) public payable {
             uint256 supply = totalSupply();
@@ -32,10 +37,16 @@ contract Collection is ERC721Enumerable, Ownable {
             require(_mintAmount <= maxMintAmount);
             require(supply + _mintAmount <= maxSupply);
             
+            if (msg.sender != owner()) {
+            require(msg.value == cost * _mintAmount, "Need to send 0.005 ether!");
+            }
+
             for (uint256 i = 1; i <= _mintAmount; i++) {
                 _safeMint(_to, supply + i);
             }
     }
+
+    
 
 
         function walletOfOwner(address _owner)
@@ -73,6 +84,10 @@ contract Collection is ERC721Enumerable, Ownable {
         
         function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner() {
             maxMintAmount = _newmaxMintAmount;
+        }
+        function setListingPrice(uint256 newPrice) public onlyOwner{
+            require(newPrice>0,"Price must be at least 1 wei");
+            cost=newPrice;
         }
         
         function setBaseURI(string memory _newBaseURI) public onlyOwner() {
